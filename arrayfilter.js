8e43f38389bee6dc5f4fc14c2fe813090d5422f0
LEELA VENKATA SAI KIRAN KUKKALA <lvskiran10@gmail.com>
2025-09-24 14:32:08 +0530

    Add files via upload

---
+++ arrayfilter.js
@@ -0,0 +1,40 @@
+// const arr = [5, 4, 6, 7, 9, 2]
+// function isOdd(x){
+//     return x % 2
+// }
+  
+// const output = arr.filter(isOdd)
+// console.log(output)
+
+// const arr = [5, 4, 6, 7, 9, 2];
+// function isEven(x){
+//     return x % 2===0
+// }
+
+// const output = arr.filter(isEven)
+// console.log(output)
+
+
+const arr = [5, 4, 6, 7, 9, 2];
+const ouput = arr.filter(x => x % 2);
+console.log(ouput);
+
+
+
+
+
+
+
+
+// < >
+
+// const arr = [5, 4, 6, 7, 9, 2];
+// function greaterThan4(x){
+//     return x > 4
+// }
+
+
+// const output = arr.filter(greaterThan4);
+// console.log(output)
+
+//((x) => x > 4)
